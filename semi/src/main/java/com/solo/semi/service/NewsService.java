75c5dd23c85c654e3e91d7eb2de26922320e64c6
codefatal <eziofatal@gmail.com>
2023-07-26 15:08:56 +0900

    사이트 api, 위젯 및 크롤링 연결
    메인 화면 구성 - 시장 요약 항목 추가
    코인시세정보 메뉴 구성 - 트레이딩뷰 차트 삽입
    뉴스 메뉴 구성 - 트레이딩뷰 뉴스 페이지 크롤링
    코인 등락폭 정보 메뉴 구성 - 트레이딩뷰 크립토 코인 마켓 위젯 삽입
    헤더 / 푸터 수정 및 레이아웃 분할
    헤더 부분 코인 티커테이프 모양 구현

---
+++ semi/src/main/java/com/solo/semi/service/NewsService.java
@@ -0,0 +1,41 @@
+package com.solo.semi.service;
+
+import com.solo.semi.News;
+import lombok.RequiredArgsConstructor;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+public class NewsService {
+    private static String News_URL = "https://kr.tradingview.com/news/";
+
+    @PostConstruct
+    public List<News> getNewsDatas() throws IOException {
+        System.setProperty("https.protocols", "TLSv1.2");
+        List<News> newsList = new ArrayList<>();
+        Document document = Jsoup.connect(News_URL).get();
+
+        Elements contents = document.select("div.container-lu7Cy9jC div.grid-ScDiRUwB a");
+
+        for(Element content : contents) {
+            News news= News.builder()
+                    .url(content.select("a").attr("abs:href"))
+                    .time(content.select("article div div").text())
+                    .subject(content.select("article > div > span").text())
+                    .build();
+            newsList.add(news);
+        }
+
+        return newsList;
+    }
+
+}
--- semi/src/test/java/com/solo/semi/SemiApplicationTests.java
+++ semi/src/test/java/com/solo/semi/SemiApplicationTests.java
@@ -1,10 +1,14 @@
 package com.solo.semi;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SemiApplicationTests {
+
+	@Autowired
+	private MemberMapper memberMapper;
 
 	@Test
 	void contextLoads() {
