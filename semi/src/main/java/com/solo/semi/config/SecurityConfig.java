cdd0b817e331d437709ff168ad968e66075e10cf
codefatal <eziofatal@gmail.com>
2023-07-26 17:20:53 +0900

    회원가입 / 로그인 설정 추가
    회원가입 / 로그인 템플릿 추가
    회원가입 시 테이블에 정보 추가
    로그인 시 테이블의 정보 확인
    로그인 페이지에서 회원가입 페이지 이동 기능 추가
    회원가입 시 동일한 id 등록 불가 설정
    회원가입 시 모든 입력폼에 내용 입력해야 동작하도록 설정
    로그인 시 아이디/패스워드 확인 절차 동작

--- semi/src/main/java/com/solo/semi/config/SecurityConfig.java
+++ semi/src/main/java/com/solo/semi/config/SecurityConfig.java
@@ -9,7 +9,6 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
@@ -21,11 +20,6 @@ public class SecurityConfig {
         http
                 .authorizeHttpRequests((authorizeHttpRequests) -> authorizeHttpRequests
                         .requestMatchers(new AntPathRequestMatcher("/**")).permitAll())
-                .csrf((csrf) -> csrf
-                        .ignoringRequestMatchers(new AntPathRequestMatcher("/h2-console/**")))
-                .headers((headers) -> headers
-                        .addHeaderWriter(new XFrameOptionsHeaderWriter(
-                                XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN)))
                 .formLogin((formLogin) -> formLogin
                         .loginPage("/user/login")
                         .defaultSuccessUrl("/"))
